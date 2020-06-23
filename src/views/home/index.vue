7fa98c9b28fbe4fa5c4d5c2e460a8f33d46a3d5d
FaithGit <futinghao1997@outlook.com>
2020-06-23 16:59:59 +0800

    fix:用户权限分配首页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -19,6 +19,18 @@ export default {
     setTimeout(function() {
       if (that.roles[0] === 'qy') {
         that.$router.replace({ name: 'Dashboard' })
+      } else if (that.roles[0] === 'gly') {
+        that.$router.replace({ name: 'Company' })
+      } else if (that.roles[0] === 'kf') {
+        that.$router.replace({ name: 'Company' })
+      } else if (that.roles[0] === 'yw') {
+        that.$router.replace({ name: 'Table' })
+      } else if (that.roles[0] === 'sjfx') {
+        that.$router.replace({ name: 'Table' })
+      } else if (that.roles[0] === 'az') {
+        that.$router.replace({ name: 'Formlist' })
+      } else if (that.roles[0] === 'kf') {
+        that.$router.replace({ name: 'Pandect' })
       }
     }, 100)
   },
